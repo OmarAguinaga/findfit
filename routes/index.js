21c646fa2e913916e1a43f35f635aa15be7ccf91
Omar Aguinaga <omar.aguinaga94@gmail.com>
2017-10-17 20:02:34 -0500

    edit user profile

--- routes/index.js
+++ routes/index.js
@@ -53,4 +53,6 @@ router.post('/login', authController.login);
 
 router.get('/logout', authController.logout);
 
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
 module.exports = router;
